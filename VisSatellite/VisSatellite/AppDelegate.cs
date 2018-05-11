8e4204f373403a6615db0d366ee3e1fbc115e862
Julius Zint <julius.zint@awin-software.de>
2018-05-11 13:21:06 +0200

    opentk sample running

--- VisSatellite/VisSatellite/AppDelegate.cs
+++ VisSatellite/VisSatellite/AppDelegate.cs
@@ -5,7 +5,9 @@ namespace VisSatellite
 {
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
-    {        
+    {
+		private MainWindowController mainWindowController;
+
 		public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender)
 		{
 			return true;
@@ -14,8 +16,10 @@ namespace VisSatellite
         public override void DidFinishLaunching(NSNotification notification)
         {
             // Insert code here to initialize your application
+			this.mainWindowController = new MainWindowController();
+            this.mainWindowController.Window.MakeKeyAndOrderFront(this);
         }
-
+       
         public override void WillTerminate(NSNotification notification)
         {
             // Insert code here to tear down your application
---
+++ VisSatellite/VisSatellite/MainWindow.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using Foundation;
+using AppKit;
+
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Platform.MacOS;
+using System.Drawing;
+
+namespace VisSatellite
+{
+    public partial class MainWindow : NSWindow
+    {
+		public MonoMacGameView Game { get; set; }
+
+        public MainWindow(IntPtr handle) : base(handle)
+        {
+        }
+
+        [Export("initWithCoder:")]
+        public MainWindow(NSCoder coder) : base(coder)
+        {
+        }
+
+		public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+
+			// Create new Game View and replace the window content with it
+            Game = new MonoMacGameView(ContentView.Frame);
+            ContentView = Game;
+
+            // Wire-up any required Game events
+            Game.Load += (sender, e) => {
+                // Initialize settings, load textures and sounds here
+            };
+
+            // Adjust the GL view to be the same size as the window
+            Game.Resize += (sender, e) => GL.Viewport(0, 0, Game.Size.Width, Game.Size.Height);
+
+            Game.UpdateFrame += (sender, e) => {
+                // Add any game logic or physics
+            };
+
+            Game.RenderFrame += (sender, e) => {
+                // Setup buffer
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                GL.MatrixMode(MatrixMode.Projection);
+
+                // Draw a simple triangle
+                GL.LoadIdentity();
+                GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+                GL.Begin(BeginMode.Triangles);
+                GL.Color3(Color.MidnightBlue);
+                GL.Vertex2(-1.0f, 1.0f);
+                GL.Color3(Color.SpringGreen);
+                GL.Vertex2(0.0f, -1.0f);
+                GL.Color3(Color.Ivory);
+                GL.Vertex2(1.0f, 1.0f);
+                GL.End();
+            };
+
+            // Run the game at 60 updates per second
+            Game.Run(60.0);
+        }
+    }
+}
