f3806a65620d55cdc6bc4211c79f5bdbeff4391c
Julius Zint <julius.zint@awin-software.de>
2018-05-11 14:07:32 +0200

    extracted drawing logic into shared code project

--- VisSatellite/vissatellite.mac/MainWindow.cs
+++ VisSatellite/vissatellite.mac/MainWindow.cs
@@ -7,11 +7,14 @@ using OpenTK.Graphics.OpenGL;
 using OpenTK.Platform.MacOS;
 using System.Drawing;
 
+using vissatellite.shared;
+
 namespace vissatellite.mac
 {
     public partial class MainWindow : NSWindow
     {
 		public MonoMacGameView Game { get; set; }
+		public VisSatteliteGame GameLogic { get; set; }
 
         public MainWindow(IntPtr handle) : base(handle)
         {
@@ -27,12 +30,14 @@ namespace vissatellite.mac
             base.AwakeFromNib();
 
 			// Create new Game View and replace the window content with it
-            Game = new MonoMacGameView(ContentView.Frame);
+            this.Game = new MonoMacGameView(ContentView.Frame);
+			GameLogic = new VisSatteliteGame();
             ContentView = Game;
 
             // Wire-up any required Game events
             Game.Load += (sender, e) => {
-                // Initialize settings, load textures and sounds here
+				// Initialize settings, load textures and sounds here
+				GameLogic.Load();
             };
 
             // Adjust the GL view to be the same size as the window
@@ -43,21 +48,8 @@ namespace vissatellite.mac
             };
 
             Game.RenderFrame += (sender, e) => {
-                // Setup buffer
-                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-                GL.MatrixMode(MatrixMode.Projection);
+				GameLogic.Render();
 
-                // Draw a simple triangle
-                GL.LoadIdentity();
-                GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
-                GL.Begin(BeginMode.Triangles);
-                GL.Color3(Color.MidnightBlue);
-                GL.Vertex2(-1.0f, 1.0f);
-                GL.Color3(Color.SpringGreen);
-                GL.Vertex2(0.0f, -1.0f);
-                GL.Color3(Color.Ivory);
-                GL.Vertex2(1.0f, 1.0f);
-                GL.End();
             };
 
             // Run the game at 60 updates per second
